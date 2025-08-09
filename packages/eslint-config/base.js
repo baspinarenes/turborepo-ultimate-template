e3bf4e03686f16cfcb8d830aa85e7167e2b95bcb
Enes Başpınar <ensbaspinar@gmail.com>
2025-08-09 14:48:15 +0300

    refactor(eslint-config): remove only warn plugin

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import onlyWarn from "eslint-plugin-only-warn";
 import turboPlugin from "eslint-plugin-turbo";
 import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
@@ -15,11 +14,6 @@ export default defineConfig([
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
-  {
-    plugins: {
-      onlyWarn,
     },
   },
   {
